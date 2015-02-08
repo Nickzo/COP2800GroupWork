6e412c7e8cb3e5896a26f4a58cea01c69af873bf
davidahincapie <davidahincapie@gmail.com>
2015-02-08 01:38:31 -0500

    Update displayMenu.java

--- src/com/GoupB/displayMenu.java
+++ src/com/GoupB/displayMenu.java
@@ -35,10 +35,82 @@ public class displayMenu {
         char b = a.next();
         return b;
     }
-
+    
+    
+//Not sure if this is where you want it
+    import java.util.Scanner;
     public static void phoneKeypadMenu(){
+        public static void main(String[] args) {
+		
+	System.out.print("Enter a string: ");
+	Scanner input = new Scanner(System.in);
+	String Number = input.next();
+	String output = "";
+	
+	for(int i = 0 ; i < Number.length() ; i++){
+	char ch = Character.toUpperCase(Number.charAt(i));//converts lowercase to uppercase and returns char value
+		if(Character.isLetter(ch)){//determines if character is a letter
+			int digit = getNumber(ch);//if it is a letter it runs the getNumber method
+			output = output + digit;}//end of if //character is converted and digit gets output
+		else{
+			 output = output + ch;}//end of else
+			        }//end of for
+			         
+	System.out.println(output);
+	
+	input.close();
+			    }//end of main
+	
+public static int getNumber(char upperCaseLetter){
+    int number = 0;
+	switch (upperCaseLetter) {
+	case 'A':
+	case 'B':
+	case 'C':
+	number = 2;
+	break;
+	case 'D':
+	case 'E':
+	case 'F':
+	number = 3;
+	break;
+	case 'G':
+	case 'H':
+	case 'I':
+	number = 4;
+	break;
+	case 'J':
+	case 'K':
+	case 'L':
+	number = 5;
+	break;
+	case 'M':
+	case 'N':
+	case 'O':
+	number = 6;
+	break;
+	case 'P':
+	case 'Q':
+	case 'R':
+	case 'S':
+	number = 7;
+	break;
+	case 'T':
+	case 'U':
+	case 'V':
+	number = 8;
+	break;
+	case 'W':
+	case 'X':
+	case 'Y':
+	case 'Z':
+	number = 9;
+	break;
+	}
+	return number;
+	}//end of getNumber
 
-    }
+    }//end of phonekeypads
 
     public static void EMIRPMenu(){
 
